336786e99fbf5071db63c5201698248116f0c82f
Dante Paradis <danteparadis77@gmail.com>
2024-09-27 08:37:47 -0400

    added comments, fixed possible issue where shows would be added to the newshow list if their rating changed

--- movie.py
+++ movie.py
@@ -4,23 +4,22 @@ import sqlite3
 import datetime
 from selenium.webdriver import ChromeOptions
 
-#set up sqlite database
+#Connect to sqlite 3 database containing shows table
 con  = sqlite3.connect('showDatabase.db')
 cursor = con.cursor()
 
-
 def scrape_and_insert():
-    # create a table containing data about each of the top 250 imdb shows
-    timestamp = datetime.datetime.now().strftime('%Y-%m-%d')
-    cursor.execute("CREATE TABLE IF NOT EXISTS shows(ID INTEGER PRIMARY KEY, title, score, rating, timestamp)")
+    #*Scrapes the top 250 shows from IMDB and inserts them into a table in showDatabase
+    timestamp = datetime.datetime.now().strftime('%Y-%m-%d') #Timestamp used to mark each entry
+    cursor.execute("CREATE TABLE IF NOT EXISTS shows(ID INTEGER PRIMARY KEY, title, score, rating, timestamp)") # Create a sqlite3 table with an id, title, imdb rating, maturity rating, and a timestamp
 
-    # Set up selenium web driver
-    options = ChromeOptions()
-    options.add_argument("--headless=new") #run in headless mode
-    options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36") #user agent must be set or page doesn't load?
-    driver = webdriver.Chrome(options=options)
-    url = "https://www.imdb.com/chart/toptv/?ref_=nv_tvv_250"
-    driver.get(url)
+    #Selenium webdriver setup
+    options = ChromeOptions() #Needed to set headless mode
+    options.add_argument("--headless=new") #Run webdriver in headless mode
+    options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36") #User agent needed when running in headless mode
+    driver = webdriver.Chrome(options=options) #apply options
+    url = "https://www.imdb.com/chart/toptv/?ref_=nv_tvv_250" #imdb url
+    driver.get(url) #set url
 
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);") #scroll to bottom of page to load all items
 
@@ -34,13 +33,13 @@ def scrape_and_insert():
     li_items = showList[0].findAll('li', attrs={'class':'ipc-metadata-list-summary-item sc-10233bc-0 TwzGn cli-parent'})
 
     for li in li_items:  # Iterate over each 'li'
-        title = li.findChildren('h3', attrs={'class': 'ipc-title__text'})[0].contents[0].encode('utf-8').decode('ascii', 'ignore')
-        score = li.findChildren('span', attrs={'ipc-rating-star--rating'})[0].contents[0].encode('utf-8').decode('ascii', 'ignore')
+        title = li.findChildren('h3', attrs={'class': 'ipc-title__text'})[0].contents[0].encode('utf-8').decode('ascii', 'ignore') #set title value, this needs editing later
+        score = li.findChildren('span', attrs={'ipc-rating-star--rating'})[0].contents[0].encode('utf-8').decode('ascii', 'ignore') #set imdb rating (score)
         #some ratings don't exist, so use try & except
         try:
             rating = li.findChildren('span', attrs=['sc-b189961a-8 hCbzGp cli-title-metadata-item'])[2].contents[0].encode('utf-8').decode('ascii', 'ignore')
         except:
-            rating = "NA"
+            rating = "NA" #for all shows where the maturity rating is blank
         
         #format title (remove leading numbers & space)
         title = title[title.index(' ')+1:]
@@ -49,34 +48,49 @@ def scrape_and_insert():
         params = (title, score, rating, timestamp)
         cursor.execute("INSERT INTO shows(title, score, rating, timestamp) VALUES (?, ?, ?, ?)", params)
         con.commit()
-    driver.quit()
+    driver.quit() #quit webdriver
 
 def compare_data():
-    today = datetime.datetime.now().strftime('%Y-%m-%d')
-    yesterday = (datetime.datetime.now() - datetime.timedelta(days=1)).strftime('%Y-%m-%d')
+    '''
+    Compares imdb tv show rankings from yesterday to today
 
-    cursor.execute('SELECT id, title, score, rating FROM shows WHERE timestamp = ?', (today,))
-    today_data = cursor.fetchall()
+    Returns
+    -------
+    2d list containing the name of a show and ranking increase (0 for a new show)
+    '''
 
-    cursor.execute('SELECT id, title, score, rating FROM shows WHERE timestamp = ?', (yesterday,))
-    yesterday_data = cursor.fetchall()
+    today = datetime.datetime.now().strftime('%Y-%m-%d') #Set the value for today in the same format as the table
+    yesterday = (datetime.datetime.now() - datetime.timedelta(days=1)).strftime('%Y-%m-%d') #Set the value for yesterday in the same format as the table
 
-    today_set = {(title, score, rating) for _, title, score, rating in today_data}
-    yesterday_set = {(title, score, rating) for _, title, score, rating in yesterday_data}
+    cursor.execute('SELECT id, title, score, rating FROM shows WHERE timestamp = ?', (today,)) #Get all tv shows from today
+    today_data = cursor.fetchall() #Store in a variable
 
-    newShows = list(today_set - yesterday_set)
+    cursor.execute('SELECT id, title, score, rating FROM shows WHERE timestamp = ?', (yesterday,)) #Get all tv shows from yesterday
+    yesterday_data = cursor.fetchall() #Store in a variable
 
+    #*Create sets from today and yesterday data without IDs or IMDB ratings to find all new shows on the top 250 list
+    today_set = {(title, rating) for _, title, rating in today_data}
+    yesterday_set = {(title, rating) for _, title, rating in yesterday_data}
+
+    newShows = list(today_set - yesterday_set) # ? does this add shows that were added and removed to the newShows list?
+
+    changes = []
+    #*Iterate through today_data and determine if a show moved positions
     for today_show in today_data:
-        id, title, score, rating = today_show
-        if (title, score, rating) not in newShows:
-            cursor.execute('SELECT id, title, score, rating FROM shows WHERE timestamp = ? AND title = ?', (yesterday, title))
+        id, title, rating = today_show #
+        if (title, rating) not in newShows:
+
+            #find the equivalent show from yesterday
+            cursor.execute('SELECT id, title, rating FROM shows WHERE timestamp = ? AND title = ?', (yesterday, title))
             yesterday_show = cursor.fetchone()
-            if yesterday_show:
-                if (title, score, rating) != (yesterday_show[1], yesterday_show[2], yesterday_show[3]):
+
+            if yesterday_show: #if there is an equivalent show for yesterday #? is this necessary
+                if (title, rating) != (yesterday_show[1], yesterday_show[3]):
                     rank_difference = (id) - (yesterday_show[0]+500)
-                    print(f"{title} (ID: {id}) moved {rank_difference} positions")
+                    changes += [title, rank_difference]
     
-    print(newShows)
+    return changes + newShows
+
 def remove_duplicates():
     cursor.execute('''
         DELETE FROM shows
@@ -89,4 +103,5 @@ def remove_duplicates():
 
     con.commit()
 
+scrape_and_insert()
 con.close()
